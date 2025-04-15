122ce84a6cbbe35f6a47b8f61bbf9b28ea49f717
PhilipTNS <124249759+PhilipTNS@users.noreply.github.com>
2025-04-15 20:23:49 +0700

    Update main.java

--- main.java
+++ main.java
@@ -1,1 +1,21 @@
+import java.util.Scanner;
+
+public class Password {
+    public static void main(String[] args){
+        Scanner input = new Scanner(System.in);
+        String password ;
+        int tries = 0 ;
+        do{
+            tries++;
+            System.out.println("What's the password?");
+            password = input.nextLine();
+        }while(!password.equals("equals") && tries <  3 );
+        if(password.equals("equals")){
+            System.out.println("correct password yippie");
+        }else{
+            System.out.println("wrong password device has been disabled for 3 days");
+        }
+        
+    }
+    }
 
